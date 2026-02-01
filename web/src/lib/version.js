efe1a6307525cb08eaa38d709c8799c2099ffc6c
Maxim Vasilkov <maxim.vasilkov@gmail.com>
2026-02-02 09:04:14 +0930

    Chore: remove unused legacy pages

--- web/src/lib/version.js
+++ web/src/lib/version.js
@@ -1,4 +1,4 @@
 // Auto-generated at build time.
 // See package.json "build" script.
 
-export const APP_VERSION = "0.1.0+c2d560b-2026-02-01T23:12:03.394Z";
+export const APP_VERSION = "0.1.0+b90f1a3-2026-02-01T23:33:31.259Z";
