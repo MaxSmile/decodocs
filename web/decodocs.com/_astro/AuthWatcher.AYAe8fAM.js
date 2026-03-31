9f8bd4cd868e59241f652adf0b2bb42ea907f05e
Maxim Vasilkov <maxim.vasilkov@gmail.com>
2026-03-31 15:00:47 +0930

    Remove uses-cases page from the web application

--- web/decodocs.com/_astro/AuthWatcher.AYAe8fAM.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index.DFBxCVLV.js";import{s as r}from"./authStore.DITxf45p.js";const o=()=>(t.useEffect(()=>{r()},[]),null);export{o as default};
--- web/decodocs.com/_astro/ReactAppIsland.CmlL4Bt5.js
+++
@@ -1,1 +0,0 @@
-import"./jsx-runtime.u17CrQMm.js";import"./index.DFBxCVLV.js";import{R as p}from"./ReactAppIsland.hw5grJpT.js";export{p as default};
